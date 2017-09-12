1991fbf235086c6b789ab471073fcb58aebc8afd
Ethan <ethan@eyeballdivision.com>
2017-09-12 16:40:01 -0600

    getting it live

--- src/app/routes/Home/About/index.js
+++ src/app/routes/Home/About/index.js
@@ -15,7 +15,7 @@ export default class About extends Component {
         <div className='lander-content'>
           <div className='hero-content-1'>
             <h2 className="tk-brandon-grotesque">About Me</h2>
-            <h6 className="tk-brandon-grotesque">Duis cillum nisi ullamco consequat veniam qui Lorem cupidatat non consectetur do irure do Lorem. Culpa reprehenderit aliquip non laborum elit excepteur. Eiusmod veniam enim laboris consectetur esse velit anim esse eiusmod do magna in mollit in.</h6>
+            <h6 className="tk-brandon-grotesque">Fullstack Engineer by day.  Adventurer by night.</h6>
           </div>
         </div>
       </div>
