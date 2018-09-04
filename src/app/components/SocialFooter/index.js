0f15fb1d3da1f798eb76e466cc2aa4aa2526fcfc
Ethan Wolz <ethan@creadiv.io>
2018-09-04 08:07:11 -0600

    Update github

--- src/app/components/SocialFooter/index.js
+++ src/app/components/SocialFooter/index.js
@@ -13,7 +13,7 @@ export default class SocialFooter extends Component {
     return (
       <div className='social-footer-bar'>
         <div className='social-footer-container'>
-          <IconLink icon={GithubIcon} href='https://github.com/wolzethan' alt='github' />
+          <IconLink icon={GithubIcon} href='https://github.com/wolzey' alt='github' />
           <IconLink icon={Facebook} href='https://facebook.com/ethan.wolz' alt='facebook' />
           <IconLink icon={Instagram} href='https://instagram.com/ethanwolz' alt='instagram' />
         </div>
